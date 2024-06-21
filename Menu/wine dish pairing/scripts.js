1721660668679fe37bf296167a79171879edc143
yva <gitauenjeri@gmail.com>
2024-06-21 17:15:26 +0300

    Worked on functionality

---
+++ Menu/wine dish pairing/scripts.js
@@ -0,0 +1,7 @@
+const backHome = document.getElementById("back-home");
+
+function navigationHome() {
+  window.location.href = "../../homepage/homepage.html";
+}
+
+backHome.addEventListener("click", navigationHome);
