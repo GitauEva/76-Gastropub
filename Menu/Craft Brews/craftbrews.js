dcf337772b06f80055be1d92bb66957c277a929d
yva <gitauenjeri@gmail.com>
2024-06-27 17:14:28 +0300

    Added a Reservations Button

--- Menu/Craft Brews/craftbrews.js
+++ Menu/Craft Brews/craftbrews.js
@@ -1,7 +1,14 @@
 const backHomeBtn = document.getElementById("back-home");
+const reservationBtn = document.getElementById("reservations");
 
 function navigateHome() {
   window.location.href = "../../homepage/homepage.html";
 }
 
 backHomeBtn.addEventListener("click", navigateHome);
+
+function reservationFunction() {
+  window.location.href = "../../Reservations/reservations.html";
+}
+
+reservationBtn.addEventListener("click", reservationFunction);
