8f6026227bdb8e37160f60936f6d0cfc018aaa3e
yva <gitauenjeri@gmail.com>
2024-06-27 21:35:16 +0300

    Added a reservations button

--- Menu/wine dish pairing/scripts.js
+++ Menu/wine dish pairing/scripts.js
@@ -1,7 +1,14 @@
 const backHome = document.getElementById("back-home");
+const reservationsBtn = document.getElementById("reservations");
 
 function navigationHome() {
   window.location.href = "../../homepage/homepage.html";
 }
 
 backHome.addEventListener("click", navigationHome);
+
+function reservationsFunction() {
+  window.location.href = "../../Reservations/reservations.html";
+}
+
+reservationsBtn.addEventListener("click", reservationsFunction);
